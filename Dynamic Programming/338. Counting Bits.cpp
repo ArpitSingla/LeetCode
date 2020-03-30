247ab8f15f125353a462a233147fd90daef608b9
Arpit Singla <arpitsingla1999@gmail.com>
2020-03-30 10:00:02 +0530

    Check Bit Manipulation Solution

--- Dynamic Programming/338. Counting Bits.cpp
+++ Dynamic Programming/338. Counting Bits.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<int> countBits(int num) {
-        vector<int> ans;
-        int *dp=new int[num+1];
-        ans.push_back(0);
+        vector<int> ans(num+1,0);
+        ans[0]=0;
         if(num==0){
             return ans;
         }
         int count=0;
         int count1=count-1;
-        dp[0]=0;
         for(int i=1;i<=num;i++){
             int temp=pow(2,count);
             if(i==temp){
                 count++;
                 count1++;
-                dp[i]=1;
+                ans[i]=1;
             }
             else{
                 int var=pow(2,count1);
-                dp[i]=dp[i-var]+1;
+                ans[i]=ans[i-var]+1;
             }
-            ans.push_back(dp[i]);
         }
         return ans;
     }

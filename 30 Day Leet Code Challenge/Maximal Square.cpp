243d4d34811b6be16d78c226f2a866f396a8ed5a
ArpitSingla <arpitsingla1999@gmail.com>
2020-04-27 23:53:12 +0530

    Day 27

---
+++ 30 Day Leet Code Challenge/Maximal Square.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+	int maximalSquare(vector<vector<char>>& matrix) {
+		if(matrix.size()==0) return 0;
+		int m=matrix.size();
+		int n=matrix[0].size();
+		vector<vector<int>> dp(m,vector<int>(n));
+		int ans=0;
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				dp[i][j]=matrix[i][j]-'0';
+				if(dp[i][j]==0) continue;
+				if(i==0 || j==0){}
+				else{
+					dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+				}
+
+				ans=max(ans,dp[i][j]*dp[i][j]);
+
+			}
+		}
+        
+		return ans;
+
+	}
+};

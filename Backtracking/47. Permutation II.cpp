8b295fd81985d6a161e6cf709b5f8d09ee65c892
ArpitSingla <arpitsingla1999@gmail.com>
2020-06-19 13:18:27 +0530

    Using Maps to deal with duplicate elements

--- Backtracking/47. Permutation II.cpp
+++ Backtracking/47. Permutation II.cpp
@@ -39,4 +39,31 @@ public:
         rec(result,temp,nums,used);
         return result;
     }
+
+    //Tushar roy way using Maps
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int>> v;
+        vector<int> r;
+        map<int, int> map;
+        for (int i : num){
+            map[i]++;
+        }
+        permuteUnique(v, r, map, num.size());
+        return v;
+    }
+
+    void permuteUnique(vector<vector<int>> &v, vector<int> &r, map<int, int> &map, int n){
+        if (n <= 0){
+            v.push_back(r);
+            return;
+        }
+        for (auto &p : map){
+            if (p.second <= 0) continue;
+            p.second--;
+            r.push_back(p.first);
+            permuteUnique(v, r, map, n - 1);
+            r.pop_back();
+            p.second++;
+        }
+    }
 };

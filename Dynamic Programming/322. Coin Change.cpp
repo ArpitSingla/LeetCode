b4e4a511770d8bfe141ff4ef3b20c561b74a3b61
ArpitSingla <arpitsingla1999@gmail.com>
2020-06-24 23:00:44 +0530

    Another Importnat Concept to lookback later

--- Dynamic Programming/322. Coin Change.cpp
+++ Dynamic Programming/322. Coin Change.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
+    //Using 2D Array
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        if(n==0){
-            return -1;
+        if(amount==0){
+            return 0;
         }
-        vector<int> dp(amount+1,INT_MAX);
-        sort(coins.begin(),coins.end());
-        dp[0]=0;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<n;j++){
-                if(coins[j]<=i){
-                    if(dp[i-coins[j]]!=INT_MAX){
-                        dp[i]=min(dp[i],dp[i-coins[j]]+1);
-                    }
+        vector<vector<unsigned int>> dp(n+1,vector<unsigned int>(amount+1,INT_MAX));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j>=coins[i-1]){
+                    dp[i][j]=min(dp[i-1][j],1+dp[i][j-coins[i-1]]);
                 }
                 else{
-                    break;
+                    dp[i][j]=dp[i-1][j];
                 }
             }
         }
-        if(dp[amount]==INT_MAX){
-            return -1;
+        return dp[n][amount]==INT_MAX?-1:dp[n][amount];
+    }
+
+    //Using 1D Array
+    int coinChange(vector<int>& coins, int amount) {
+        int n=coins.size();
+        if(amount==0){
+            return 0;
         }
-        return dp[amount];
+        vector<int> dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int j=1;j<=amount;j++){
+            for(auto i:coins){
+                if(j>=i){
+                    dp[j]=min(dp[j],1+dp[j-i]);
+                }
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
     }
 };
